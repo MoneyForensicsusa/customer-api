bd82978c13fddc6bfde92b0f736fc58edc927489
Wonderful Eyeh <wonderfuleyeh25@gmail.com>
2026-06-25 17:08:19 -0500

    created conftest file which will be used by the pytest functions

---
+++ conftest.py
@@ -0,0 +1,15 @@
+import pytest
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+@pytest.fixture
+def test_customer():
+    data = {"email": "alpha.beta@gmail.com", "name": "Alpha beta", "city": "Austin"}
+    response = client.post('/customers/', json=data)
+    created = response.json()
+
+    yield created
+
+    client.delete(f'/customers/{created["id"]}')
